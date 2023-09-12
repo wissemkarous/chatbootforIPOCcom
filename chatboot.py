76b673802a9ddb40550aae896689c85d6dbe2adb
Ryuzaki ⚡ <115191512+wissemkarous@users.noreply.github.com>
2023-09-12 14:26:38 +0100

    Add files via upload

---
+++ chatboot.py
@@ -0,0 +1,46 @@
+import json
+import re
+import random_responses
+
+# Load JSON data
+def load_json(file):
+    with open(file) as bot_responses:
+        print(f"Loaded '{file}' successfully!")
+        return json.load(bot_responses)
+
+# Store JSON data
+response_data = load_json("bot.json")
+
+def get_response(input_string):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', input_string.lower())
+    score_list = []
+
+    # Check all the responses
+    for response in response_data:
+        response_score = 0
+
+        # Check if any user input words match
+        for word in split_message:
+            if word in response["user_input"]:
+                response_score += 1
+
+        # Add score to list
+        score_list.append(response_score)
+
+    # Find the best response and return it if they're not all 0
+    best_response = max(score_list)
+    response_index = score_list.index(best_response)
+
+    # Check if input is empty
+    if input_string == "":
+        return "Please type something so we can chat :("
+
+    # If there is no good response, return a random one.
+    if best_response != 0:
+        return response_data[response_index]["bot_response"]
+
+    return random_responses.random_string()
+
+while True:
+    user_input = input("You: ")
+    print("Bot:", get_response(user_input))
